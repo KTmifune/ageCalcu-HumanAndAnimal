396189e80805142c32adf0ed57b86b89f63ea93a
Takuma Kato <k.t.mifune@gmail.com>
2020-05-30 16:47:16 +0900

    カレンダーから取得した年月日からそれまでの人生の分時間を割り出し、表示する

--- app/src/main/java/cowma/katotama/agecalchumanandanimal/MainActivity.kt
+++ app/src/main/java/cowma/katotama/agecalchumanandanimal/MainActivity.kt
@@ -4,8 +4,11 @@ import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.CalendarView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,12 +18,44 @@ class MainActivity : AppCompatActivity() {
 
         btnDataPicker.setOnClickListener {view ->
             clickDatePicker(view)
-            Toast.makeText(this,"Button works", Toast.LENGTH_SHORT).show()
         }
     }
 
     fun clickDatePicker(view: View){
-        DetePickeDialog(this,DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->  })
+
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val dayOfMonth = myCalendar.get(Calendar.DAY_OF_MONTH)
+
+        val dpd = DatePickerDialog(
+            this,DatePickerDialog.OnDateSetListener {
+                    view, selectedYear, selectedMonth, selectedDay ->
+                val selectedDate = "${selectedYear}/${selectedMonth+1}/${selectedDay}"
+               textDate.setText(selectedDate)
+
+                //born date
+                val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+                val theDate = sdf.parse(selectedDate)
+                Toast.makeText(this,"the date is $sdf", Toast.LENGTH_LONG).show()
+
+                val selectedDateInMinute = theDate!!.time
+
+                //current date
+                val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+                val currentDateInMinute = currentDate!!.time
+
+                //differenceDate
+                val differenceMinute = (currentDateInMinute - selectedDateInMinute) / (1000 * 60 * 60 * 24 )
+
+                textDifferenceInMinute.setText(differenceMinute.toString())
+
+            }
+            ,year,month,dayOfMonth)
+
+
+        dpd.datePicker.setMaxDate(Date().time - 86400000)
+        dpd.show()
 
     }
 }
